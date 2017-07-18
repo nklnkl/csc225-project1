084743f49bfeb935a586faca5997f5d3c7661d63
Niko Lagman <Lagman.Niko@gmail.com>
2017-07-18 15:37:46 -0400

    Add nav button color changing feature

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 $(document).ready(function () {
+  // Path endpoint of url
+  var path = window.location.pathname;
+
+  // To change the color of a nav button based on url
+  switch (path) {
+    case '/index.html':
+      $('header > nav > a:nth-of-type(1)').addClass('active');
+      break;
+    case '/about.html':
+      $('header > nav > a:nth-of-type(2)').addClass('active');
+      break;
+    case '/gallery.html':
+      $('header > nav > a:nth-of-type(3)').addClass('active');
+      break;
+    case '/contact.html':
+      $('header > nav > a:nth-of-type(4)').addClass('active');
+      break;
+  }
+
 });
